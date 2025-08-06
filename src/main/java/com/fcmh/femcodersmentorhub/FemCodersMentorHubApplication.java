aa3ab12c07004960e385a8b6ad9f4e10c04483b7
Lara Pla <laraplamoreno@gmail.com>
2025-08-06 12:23:11 +0200

    build: add project structure and dependencies

---
+++ src/main/java/com/fcmh/femcodersmentorhub/FemCodersMentorHubApplication.java
@@ -0,0 +1,13 @@
+package com.fcmh.femcodersmentorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemCodersMentorHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FemCodersMentorHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcmh/femcodersmentorhub/FemCodersMentorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcmh.femcodersmentorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemCodersMentorHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
