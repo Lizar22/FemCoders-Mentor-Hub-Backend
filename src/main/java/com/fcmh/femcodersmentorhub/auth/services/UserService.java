dc6b436f68b5b7eb9540900dcef2397b88f8b1c3
Lara Pla <laraplamoreno@gmail.com>
2025-08-07 00:16:33 +0200

    feat(UserRepository, UserDetail, UserService): add attributes and methods

---
+++ src/main/java/com/fcmh/femcodersmentorhub/auth/services/UserService.java
@@ -0,0 +1,16 @@
+package com.fcmh.femcodersmentorhub.auth.services;
+
+import com.fcmh.femcodersmentorhub.auth.User;
+import com.fcmh.femcodersmentorhub.auth.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User user)
+}
