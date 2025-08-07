85440074badf71caac7356fee74a1859ff16522f
Lara Pla <laraplamoreno@gmail.com>
2025-08-08 00:10:36 +0200

    refactor(UserAuthService): remove unused methods

--- src/main/java/com/fcmh/femcodersmentorhub/auth/services/UserAuthService.java
+++ src/main/java/com/fcmh/femcodersmentorhub/auth/services/UserAuthService.java
@@ -3,13 +3,7 @@ package com.fcmh.femcodersmentorhub.auth.services;
 import com.fcmh.femcodersmentorhub.auth.dtos.UserAuthRequest;
 import com.fcmh.femcodersmentorhub.auth.dtos.UserAuthResponse;
 
-import java.util.List;
-
 public interface UserAuthService {
     UserAuthResponse findUserById(Long id);
     UserAuthResponse addUser(UserAuthRequest userRequest);
-    UserAuthResponse authenticateUser(String email, String password);
-    /*List<UserAuthResponse> findAllUsers();
-    UserAuthResponse updateUser(Long id, UserAuthRequest userRequest);
-    void deleteUser(Long id);*/
 }
---
+++ src/main/java/com/fcmh/femcodersmentorhub/security/CustomUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.fcmh.femcodersmentorhub.security;
+
+import com.fcmh.femcodersmentorhub.auth.repository.UserAuthRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserAuthRepository userAuthRepository;
+
+    public CustomUserDetailsService(UserAuthRepository userAuthRepository) {
+        this.userAuthRepository = userAuthRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
