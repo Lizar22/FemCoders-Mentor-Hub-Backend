d88302f2c6126b2856c053430fa90862bca2d79a
Lara Pla <laraplamoreno@gmail.com>
2025-08-08 00:41:51 +0200

    refactor(SecurityConfig): add securityFilterChain method

--- src/main/java/com/fcmh/femcodersmentorhub/security/CustomUserDetailsService.java
+++ src/main/java/com/fcmh/femcodersmentorhub/security/CustomUserDetailsService.java
@@ -16,4 +16,5 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
     }
+
 }
---
+++ src/main/java/com/fcmh/femcodersmentorhub/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.fcmh.femcodersmentorhub.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(manager -> manager
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
