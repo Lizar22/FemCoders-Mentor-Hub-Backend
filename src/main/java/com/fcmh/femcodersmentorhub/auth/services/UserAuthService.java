adfbf9493199dd040b3543e34af58c303502521c
Lara Pla <laraplamoreno@gmail.com>
2025-08-07 20:58:53 +0200

    feat(UserAuthService/Impl): add findUserById, addUser and authenticateUser methods

--- src/main/java/com/fcmh/femcodersmentorhub/auth/services/UserAuthService.java
+++ src/main/java/com/fcmh/femcodersmentorhub/auth/services/UserAuthService.java
@@ -6,11 +6,10 @@ import com.fcmh.femcodersmentorhub.auth.dtos.UserAuthResponse;
 import java.util.List;
 
 public interface UserAuthService {
-    List<UserAuthResponse> findAllUsers();
     UserAuthResponse findUserById(Long id);
     UserAuthResponse addUser(UserAuthRequest userRequest);
+    UserAuthResponse authenticateUser(String email, String password);
+    /*List<UserAuthResponse> findAllUsers();
     UserAuthResponse updateUser(Long id, UserAuthRequest userRequest);
-    void deleteUser(Long id);
-
-
+    void deleteUser(Long id);*/
 }
--- src/main/java/com/fcmh/femcodersmentorhub/auth/services/UserAuthServiceImpl.java
+++ src/main/java/com/fcmh/femcodersmentorhub/auth/services/UserAuthServiceImpl.java
@@ -9,19 +9,32 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserAuthServiceImpl {
+public class UserAuthServiceImpl implements UserAuthService {
     private final UserAuthRepository userAuthRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final UserAuthMapper userAuthMapper;
 
-    public UserAuthServiceImpl(UserAuthRepository userAuthRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserAuthServiceImpl(UserAuthRepository userAuthRepository, BCryptPasswordEncoder passwordEncoder, UserAuthMapper userAuthMapper) {
         this.userAuthRepository = userAuthRepository;
         this.passwordEncoder = passwordEncoder;
+        this.userAuthMapper = userAuthMapper;
     }
 
+        @Override
+    public UserAuthResponse findUserById(Long id) {
+        return null;
+    }
+
+    @Override
     public UserAuthResponse addUser(UserAuthRequest userAuthRequest) {
-        UserAuth newUser = UserAuthMapper.dtoToEntity(userAuthRequest);
+        UserAuth newUser = userAuthMapper.dtoToEntity(userAuthRequest);
         newUser.setPassword(passwordEncoder.encode(userAuthRequest.password()));
         UserAuth savedUser = userAuthRepository.save(newUser);
-        return UserAuthMapper.entityToDto(savedUser);
+        return userAuthMapper.entityToDto(savedUser);
+    }
+
+    @Override
+    public UserAuthResponse authenticateUser(String email, String password) {
+        return null;
     }
 }
