2abfe1b3626210a659acf6bbc5cb46f2a236d00d
Lara Pla <laraplamoreno@gmail.com>
2025-08-10 20:30:11 +0200

    feat(UserAuthServiceImplTest): add attributes, annotations and setUp method

---
+++ src/test/java/com/fcmh/femcodersmentorhub/UserAuthServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.fcmh.femcodersmentorhub;
+
+import com.fcmh.femcodersmentorhub.auth.UserAuth;
+import com.fcmh.femcodersmentorhub.auth.dtos.register.UserAuthMapper;
+import com.fcmh.femcodersmentorhub.auth.dtos.register.UserAuthRequest;
+import com.fcmh.femcodersmentorhub.auth.dtos.register.UserAuthResponse;
+import com.fcmh.femcodersmentorhub.auth.repository.UserAuthRepository;
+import com.fcmh.femcodersmentorhub.auth.services.UserAuthServiceImpl;
+import com.fcmh.femcodersmentorhub.security.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+public class UserAuthServiceImplTest {
+
+    @Mock
+    private UserAuthRepository userAuthRepository;
+
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Mock
+    private UserAuthMapper userAuthMapper;
+
+    @InjectMocks
+    private UserAuthServiceImpl userAuthService;
+
+    private UserAuth testUserAuth;
+    private UserAuthRequest testUserAuthRequest;
+    private UserAuthResponse testUserAuthResponse;
+    private final Long TEST_USER_ID = 1L;
+    private final String TEST_USERNAME = "Cris Mouta";
+    private final String TEST_EMAIL = "cris.mouta@fcmh.com";
+    private final String TEST_PASSWORD = "Password123.";
+    private final String ENCODED_PASSWORD = "$2a$10$EncodedPasswordHash";
+    private final Role TEST_ROLE = Role.MENTOR;
+
+    @BeforeEach
+    void setUp() {
+        testUserAuth = new UserAuth();
+        testUserAuth.setId(TEST_USER_ID);
+        testUserAuth.setUsername(TEST_USERNAME);
+        testUserAuth.setEmail(TEST_EMAIL);
+        testUserAuth.setPassword(ENCODED_PASSWORD);
+        testUserAuth.setRole(TEST_ROLE);
+
+        testUserAuthRequest = new UserAuthRequest(TEST_USERNAME, TEST_EMAIL, TEST_PASSWORD, TEST_ROLE);
+        testUserAuthResponse = new UserAuthResponse(TEST_USERNAME, TEST_EMAIL, TEST_ROLE);
+    }
+
+}
