0048ead31ccbf651bcd954581ce594473a47a6a2
Lara Pla <laraplamoreno@gmail.com>
2025-08-26 13:24:03 +0200

    feat(MentorService/Impl): add basic crud

--- src/main/java/com/fcmh/femcodersmentorhub/mentors/services/MentorService.java
+++ src/main/java/com/fcmh/femcodersmentorhub/mentors/services/MentorService.java
@@ -1,8 +1,14 @@
 package com.fcmh.femcodersmentorhub.mentors.services;
 
-import com.fcmh.femcodersmentorhub.mentors.MentorProfile;
+import com.fcmh.femcodersmentorhub.mentors.dtos.MentorRequest;
+import com.fcmh.femcodersmentorhub.mentors.dtos.MentorResponse;
+
+import java.util.List;
 
 public interface MentorService {
-    MentorProfile getAllMentors();
-    MentorProfile getMentorById(Long id);
+    List<MentorResponse> getAllMentors();
+    MentorResponse getMentorProfileById(Long id);
+    MentorResponse addMentorProfile(MentorRequest mentorRequest);
+    MentorResponse updateMentorProfile(Long id, MentorRequest mentorRequest);
+    void deleteMentorProfile(Long id);
 }
--- src/main/java/com/fcmh/femcodersmentorhub/mentors/services/MentorServiceImpl.java
+++ src/main/java/com/fcmh/femcodersmentorhub/mentors/services/MentorServiceImpl.java
@@ -1,17 +1,56 @@
 package com.fcmh.femcodersmentorhub.mentors.services;
 
+import com.fcmh.femcodersmentorhub.auth.exceptions.UserNotFoundException;
 import com.fcmh.femcodersmentorhub.mentors.MentorProfile;
+import com.fcmh.femcodersmentorhub.mentors.MentorRepository;
+import com.fcmh.femcodersmentorhub.mentors.dtos.MentorMapper;
+import com.fcmh.femcodersmentorhub.mentors.dtos.MentorRequest;
+import com.fcmh.femcodersmentorhub.mentors.dtos.MentorResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MentorServiceImpl implements MentorService {
-    @Override
-    public MentorProfile getAllMentors() {
-        return null;
+    private final MentorRepository mentorRepository;
+
+    public MentorServiceImpl(MentorRepository mentorRepository) {
+        this.mentorRepository = mentorRepository;
     }
 
     @Override
-    public MentorProfile getMentorById(Long id) {
-        return null;
+    public List<MentorResponse> getAllMentors() {
+        List<MentorProfile> mentors = mentorRepository.findAll();
+        return mentors.stream().map(MentorMapper::entityToDto).toList();
+    }
+
+    @Override
+    public MentorResponse getMentorProfileById(Long id) {
+        MentorProfile mentor = mentorRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found with id " + id));
+        return MentorMapper.entityToDto(mentor);
+    }
+
+    @Override
+    public MentorResponse addMentorProfile(MentorRequest mentorRequest) {
+        MentorProfile newMentor = MentorMapper.dtoToEntity(mentorRequest);
+        MentorProfile savedMentor = mentorRepository.save(newMentor);
+        return MentorMapper.entityToDto(savedMentor);
+    }
+
+    @Override
+    public MentorResponse updateMentorProfile(Long id, MentorRequest mentorRequest) {
+        MentorProfile existingMentor = mentorRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User with id " + id + " not found"));
+        existingMentor.setFullName(mentorRequest.fullName());
+        existingMentor.setTechnologies(mentorRequest.technologies());
+        existingMentor.setLevel(mentorRequest.level());
+        existingMentor.setBio(mentorRequest.bio());
+        MentorProfile updatedMentor = mentorRepository.save(existingMentor);
+        return MentorMapper.entityToDto(updatedMentor);
+    }
+
+    @Override
+    public void deleteMentorProfile(Long id) {
+        getMentorProfileById(id);
+        mentorRepository.deleteById(id);
     }
 }
