d57cbb5db382a03b2e67471960aaf06b03f9c0f9
Lara Pla <laraplamoreno@gmail.com>
2025-08-31 17:42:48 +0200

    feat(MentoringRequestRepository): add mentoring request repository

---
+++ src/main/java/com/fcmh/femcodersmentorhub/requests/MentoringRequestRepository.java
@@ -0,0 +1,6 @@
+package com.fcmh.femcodersmentorhub.requests;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MentoringRequestRepository extends JpaRepository<MentoringRequest, Long> {
+}
