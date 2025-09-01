0c9857d84ab37f3b2472eb2134e95c18ab4a9c94
Lara Pla <laraplamoreno@gmail.com>
2025-09-01 15:09:09 +0200

    refactor(MentorMapper, UserAuthMapper): remove Component annotation

--- src/main/java/com/fcmh/femcodersmentorhub/auth/dtos/register/UserAuthMapper.java
+++ src/main/java/com/fcmh/femcodersmentorhub/auth/dtos/register/UserAuthMapper.java
@@ -1,9 +1,7 @@
 package com.fcmh.femcodersmentorhub.auth.dtos.register;
 
 import com.fcmh.femcodersmentorhub.auth.UserAuth;
-import org.springframework.stereotype.Component;
 
-@Component
 public class UserAuthMapper {
     public static UserAuth dtoToEntity(UserAuthRequest dto) {
         return UserAuth.builder()
--- src/main/java/com/fcmh/femcodersmentorhub/mentors/dtos/MentorMapper.java
+++ src/main/java/com/fcmh/femcodersmentorhub/mentors/dtos/MentorMapper.java
@@ -1,11 +1,9 @@
 package com.fcmh.femcodersmentorhub.mentors.dtos;
 
 import com.fcmh.femcodersmentorhub.mentors.MentorProfile;
-import org.springframework.stereotype.Component;
 
 import java.util.stream.Collectors;
 
-@Component
 public class MentorMapper {
     public static MentorProfile dtoToEntity(MentorRequest dto) {
         return MentorProfile.builder()
